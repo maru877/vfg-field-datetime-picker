54dd0cea71f5514e0d68b3d7d5e46f2f0459c6d6
Yuki Miyata <hq3p15ofcv@gmail.com>
2018-12-04 16:31:22 +0900

    remove unnecessary files

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,16 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue'
-import FieldAwesome from '../components/FieldAwesome.vue'
 
 storiesOf('Button', module)
-  .add('field awsome', () => ({
-    components: { FieldAwesome },
+  .add('flat pickr', () => ({
     template: '<vue-form-generator :schema="schema" :model="model" :options="formOptions">',
     data: () => ({
       schema: {
         fields: [{
           type: 'flat-pickr',
-          label: 'Awesome (custom field)',
+          label: 'flatPickr (custom field)',
           model: 'flatPickr'
         }]
       },
